8e5ea50d40b9a5fc2409651334ef19241c368515
NyanLight <128150876+NyanLight@users.noreply.github.com>
2023-08-30 23:50:19 +0400

    change grid button works now"

--- index.js
+++ index.js
@@ -5,26 +5,31 @@ const resetBtn = document.querySelector("#reset");
 const sizeBtn = document.querySelector("#size");
 const colorBtn = document.querySelector("#color");
 
-
 function createGrid(size) {
-    for (j = 0; j < (size * size); j++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.style.flexBasis = `${600 / size}px`;
-        cell.addEventListener("mouseover", () => {
-          cell.classList.add("hover");
-        });
-        container.appendChild(cell);
+  for (i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.flexBasis = `${600 / size}px`;
+    cell.addEventListener("mouseover", () => {
+      cell.classList.add("hover");
+    });
+    container.appendChild(cell);
   }
 }
 
 resetBtn.addEventListener("click", () => {
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach((cell) => {
-        cell.classList.remove('hover');
-    })
-
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.classList.remove("hover");
+  });
 });
 
+sizeBtn.addEventListener('click', () => {
+    container.innerHTML='';
+    size = Number(prompt('how many cells do you want?'));
+    createGrid(size);
+});
 
 createGrid(4);
+
+
