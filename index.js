dab71fe6a5c4e7277576383b2f51fd9ffa5ca950
NyanLight <128150876+NyanLight@users.noreply.github.com>
2023-08-30 23:30:30 +0400

    reset button works now

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 
-let size = 4; 
-const resetBtn = document.querySelector('#reset');
-const sizeBtn = document.querySelector('#size');
-const colorBtn = document.querySelector('#color');
+let size = 4;
+const resetBtn = document.querySelector("#reset");
+const sizeBtn = document.querySelector("#size");
+const colorBtn = document.querySelector("#color");
 
-
-function createRow () {
-    for (j = 0; j < size; j++ ) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.flexBasis = `${600 / size}px`;
-        cell.addEventListener('mouseover', () => {
-            cell.classList.add('hover');
-        });
-        container.appendChild(cell);
-    }
+function createRow(size = 4) {
+  for (j = 0; j < size; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.flexBasis = `${600 / size}px`;
+    cell.addEventListener("mouseover", () => {
+      cell.classList.add("hover");
+    });
+    container.appendChild(cell);
+  }
 }
 
-function createGrid () {
-    for (i = 0; i < size; i ++) {
-        createRow(); 
-    }
-};
+function createGrid(size = 4) {
+  for (i = 0; i < size; i++) {
+    createRow();
+  }
+}
 
-function eraseBackground () {
-    cell.classList.remove('hover');
-};
+resetBtn.addEventListener("click", () => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.classList.remove('hover');
+    })
 
-resetBtn.addEventListener('click', () => eraseBackground);
-
+});
 
 createGrid();
